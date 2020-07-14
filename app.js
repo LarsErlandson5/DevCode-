45f1754a515d779049c95b8448c69be113523325
unknown <larserlandson5@gmail.com>
2020-07-13 22:31:06 -0500

    adding for loop to iterate over data and log to console.

--- app.js
+++ app.js
@@ -3,7 +3,26 @@ $(document).ready(function () {
         url: "http://www.devcodecampmusiclibrary.com/api/music",
         dataType: "json",
         type: 'get',
-
+        success: function (data, testStatus, jQxhr) {
+            console.log(data[1]);
+        musicTable = buildTable(data)
+            
         }
-    })
+    });
 })
+
+function buildTable(input){
+    let table = document.getElementById('table')
+
+    for(let i=0; i<input.length; i++){
+        let row = `<tr>
+            <td>${input[i]("title")}</td>
+            <td>${input[i].album}</td>
+            <td>${input[i].artist}</td>
+            <td>${input[i].genre}</td>
+            <td>${input[i].releaseDate}</td>
+
+        </tr>`
+    }
+    }
+
